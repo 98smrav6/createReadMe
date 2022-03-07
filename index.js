f3f5bbd524489f56928904e53db7cd1d01e0972e
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2022-03-07 12:06:08 -0500

    started array of questions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-# creatReadMe
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('');
+
+// Arrays of questions
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        messsage: 'What kind of licesnse should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+]
